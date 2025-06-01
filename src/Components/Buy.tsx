28700d167d4cc0b6a3869c6a4bb7bb9e22625f8b
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2025-05-31 23:07:10 -0300

    Update Buy.tsx

--- src/Components/Buy.tsx
+++ src/Components/Buy.tsx
@@ -7,7 +7,7 @@ function Buy() {
     <div className="flex flex-col items-center justify-center">
       <button
         className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        onClick={() => navigate("/payment")}
+        onClick={() => navigate("/cart")}
       >
         Comprar
       </button>
